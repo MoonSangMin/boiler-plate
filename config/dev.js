dbce4c720f961e6891e22fb5b063517d3b5ae164
문상민 <moonsangmin@munsangmin-ui-MacBookAir.local>
2021-08-23 21:51:52 +0900

    for mongoDB security

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://moon:anstkdals10@9@bolierplate.7tlrg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const config = require('./config/key.js')
+const { User } = require("./models/User")
+
+//application/x-www-form-ulrencoded로 된 정보를 분석하여 가져옴
+app.use(bodyParser.urlencoded({extended: true}))
+
+//application/json으로 된 정보를 분석하여 가져옴
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://moon:anstkdals10@9@bolierplate.7tlrg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected..'))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello world!'))
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Port num : ${port}`))
